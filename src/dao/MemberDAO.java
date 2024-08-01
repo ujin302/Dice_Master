a6d9e051b7cab399c230dc16c2906a6380b8da06
= <=>
2024-08-01 14:46:11 +0900

    Test 파일 삭제

--- src/dao/MemberDAO.java
+++ src/dao/MemberDAO.java
@@ -57,20 +57,45 @@ public class MemberDAO extends BaseDAO {
 	}
 	
 	// 사용자 & 관리자 로그인
-	public MemberDTO loginMember(String userID, String userPW) {
-		MemberDTO dto = null;
-		sql = "select * from member where uesr_id = ?, uesr_pw =?";
+	public MemberDTO loginMember(String userID, String userPW) { // 매개변수 ID, PW 
+		/*
+		 * loginMember() 함수 사용 이유 : member(T)에서 id와 pw가 일치하는 한 행을 가져오겠다. 
+		 * 
+		 * 매개변수 필요해??? 
+		 * 
+		 * 방법 1. 매개변수 : id, pw 
+		 * 방법 2. 매개변수 : memberDTO -> 4개 : null 
+		 * 방법 3. 매개변수 : String[] = {id, pw} -> 인덱스 번호 기억  
+		 * 
+		 * 반환값 ? 
+		 * 성공 여부만 확인? >> boolean
+		 * 성공했다면 로그인 정보도 출력? -> DTO, Map, Array
+		 * 	-> 환영합니다 "ooo" 님, 현재 잔액 ~ 
+		 * 	-> MemberDTO : 해당 사용자의 모든 정보를 담고 있다.
+		 * 
+		 * 
+		 * [ 작업 내용 ]
+		 * 존재 O 
+		 * 	로그인 성공 의미
+		 * 존재 X 
+		 * 	로그인 실패 의미 
+		 * 
+		 */
 		
-		try {
-			super.con = super.getConnection(); 
-			super.pstmt = con.prepareStatement(sql);
+		MemberDTO dto = null; // ? 반환 시, 사용하는 DTO 
+		sql = "select * from member where uesr_id = ?, uesr_pw =?"; // ? 
+		
+		try { // ? 
+			super.con = super.getConnection(); // 드라이버 연결한 데이터 저장 
+			super.pstmt = con.prepareStatement(sql); // sql 컴파일 
 			
-			pstmt.setString(1, userID);
+			pstmt.setString(1, userID); // id = ? 데이터 매핑
 			pstmt.setString(2, userPW);
 			
-			rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery(); // sql 실행 & rs에 결과값 저장 
 			
-			while(rs.next()) {
+			while(rs.next()) { // rs 다음꺼 들고오기 있으면 T, 없으면 F
+				// 다음꺼 정보 저장 
 				dto = new MemberDTO(
 									rs.getString("user_Name"),
 									rs.getString("user_ID"),
@@ -82,12 +107,14 @@ public class MemberDAO extends BaseDAO {
 			}
 			
 		} catch (SQLException e) {
-			e.printStackTrace();
+			e.printStackTrace(); // e 변수에 try 예외 정보 전달. 빨간줄 출력 
 		} finally {
-			super.closeDB(con, pstmt, rs);
+			super.closeDB(con, pstmt, rs); // 편하게 가져다 쓰기 위해서 함수 호출 
 		}
 		
-		return dto;
+		
+		return dto; 
+		
 	}
 
 	// 사용자 & 관리자 탈퇴
--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -26,20 +26,6 @@ public class MemberService { // 사용자 & 관리자 공통 부분 처리
 
 	// 사용자 & 관리자 회원가입
 	public void joinMembership() {
-		/*
-		 * 사용자 : 무제한 & num == 1
-		 * 관리자 : 인원 제한 3명 & num == 2
-		 * 
-		 * [ 사용자 입력 ] 
-		 * UserName 
-		 * ID
-		 * PW 
-		 * Email 
-		 * 
-		 * PW 제외 중복 X 
-		 * 
-		 * 회원가입 후, 로그인 화면 이동 >> login(); 호출
-		 */
 		dto = new MemberDTO();
 		
 		System.out.println("안녕하세요. " + role + " 회원 가입을 위해 아래 정보를 작성해주세요.");
@@ -66,15 +52,6 @@ public class MemberService { // 사용자 & 관리자 공통 부분 처리
 	
 	// 사용자 & 관리자 로그인
 	public void login() {
-		/*
-		 * 사용자 : num == 1
-		 * 관리자 : num == 2
-		 * 
-		 * [ 사용자 입력 ] 
-		 * ID
-		 * PW 
-		 */
-		
 		System.out.println("안녕하세요. " + role + " 로그인 정보를 입력해주세요.");
         System.out.print("아이디 : ");
         String userID = sc.next();
